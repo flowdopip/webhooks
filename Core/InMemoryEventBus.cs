90242c80b071ba239fa9533ef3472bbbb58077db
Chad Lee <chad.lee@archoninfosys.com>
2015-01-20 14:28:58 -0600

    Fixing namespace in InMemoryEventBus

--- Core/InMemoryEventBus.cs
+++ Core/InMemoryEventBus.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Archon.Webhooks.Tests
+namespace Archon.Webhooks
 {
 	public class InMemoryEventBus : EventBus
 	{
