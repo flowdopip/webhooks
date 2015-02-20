e3be952425ba8528924fb286691d52c1de67ccef
Chad Lee <chad.lee@archoninfosys.com>
2015-02-20 16:48:47 -0600

    Adding boilerplate for EventController

---
+++ Core/Api/EventController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Archon.WebApi;
+
+namespace Archon.Webhooks.Api
+{
+	[RoutePrefix("hooks/{hookId:int}/events")]
+	public class EventController : ApiController
+	{
+		readonly EventBus bus;
+
+		public EventController(EventBus bus)
+		{
+			this.bus = bus;
+		}
+	}
+}
